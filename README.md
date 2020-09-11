81bc22bfce4f8a661f608759c728b9a1a3c7c86e
gilzoide <gilzoide@gmail.com>
2020-09-11 15:20:31 -0300

    Update README with new script name and usage info

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# c_extract.py
-Automatic extraction of C APIs using [clang](https://pypi.org/project/clang/).
-Exports struct/union/enum, typedefs, static variables
-and function definitions to a JSON format.
+# c_api_extract.py
+Automatic extraction of C APIs from header files using
+[Python](http://python.org/) and [clang](https://pypi.org/project/clang/).
+Exports struct/union/enum, typedefs, static variables and function definitions
+to a JSON file content.
+
+## Usage
+Calling from console:
+
+    $ c_api_extract.py [--compact] <input> [-- <clang_args>...]
+
+Or using Python:
+
+```python
+import c_api_extract
+
+definitions = c_api_extract.definitions_from_header('header_name.h', ['-Dclang_args'])
+# definitions follow the same format as output JSON
+```
+
+`c_extract.py` works on a single header file for simplicity.
+If you need more than one header processed, create a new one and `#include` them.
 
--- c_api_extract.py
+++ c_api_extract.py
@@ -1,6 +1,6 @@
 """
 Usage:
-  c_extract.py [--compact] <input> [-- <clang_args>...]
+  c_api_extract.py [--compact] <input> [-- <clang_args>...]
 
 Options:
   -c, --compact    Write minified JSON.
@@ -15,14 +15,15 @@ import clang.cindex as clang
 def verbatim_tokens(cursor):
     return ' '.join(t.spelling for t in cursor.get_tokens() if not t.spelling.startswith('/'))
 
+
 class Visitor:
     def __init__(self):
         self.defs = []
         self.typedefs = {}
+        self.index = clang.Index.create()
 
     def parse_header(self, header_path, clang_args=[]):
-        index = clang.Index.create()
-        tu = index.parse(
+        tu = self.index.parse(
             header_path,
             args=clang_args,
             options=clang.TranslationUnit.PARSE_SKIP_FUNCTION_BODIES | clang.TranslationUnit.PARSE_INCOMPLETE | clang.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD
@@ -59,7 +60,8 @@ class Visitor:
                 kind='enum',
                 name=cursor.spelling,
                 type=cursor.enum_type.spelling,
-                values=[(c.spelling, c.enum_value) for c in cursor.get_children()],
+                values=[(c.spelling, c.enum_value)
+                        for c in cursor.get_children()],
                 verbatim=verbatim_tokens(cursor),
             )
             self.defs.append(enum)
@@ -68,7 +70,8 @@ class Visitor:
             struct = dict(
                 kind='struct',
                 name=cursor.spelling,
-                fields=[(f.type.spelling, f.spelling) for f in cursor.type.get_fields()],
+                fields=[(f.type.spelling, f.spelling)
+                        for f in cursor.type.get_fields()],
                 verbatim=verbatim_tokens(cursor),
             )
             self.defs.append(struct)
@@ -77,7 +80,8 @@ class Visitor:
             union = dict(
                 kind='union',
                 name=cursor.spelling,
-                fields=[(f.type.spelling, f.spelling) for f in cursor.type.get_fields()],
+                fields=[(f.type.spelling, f.spelling)
+                        for f in cursor.type.get_fields()],
                 verbatim=verbatim_tokens(cursor),
             )
             self.defs.append(union)
@@ -87,17 +91,24 @@ class Visitor:
                 kind='function',
                 name=cursor.spelling,
                 return_type=cursor.type.get_result().spelling,
-                arguments=[(a.type.spelling, a.spelling) for a in cursor.get_arguments()],
+                arguments=[(a.type.spelling, a.spelling)
+                           for a in cursor.get_arguments()],
                 variadic=cursor.type.kind == clang.TypeKind.FUNCTIONPROTO and cursor.type.is_function_variadic(),
             )
             self.defs.append(function)
 
 
+def definitions_from_header(*args, **kwargs):
+    visitor = Visitor()
+    visitor.parse_header(*args, **kwargs)
+    return visitor.defs
+
+
 def main():
     opts = docopt(__doc__)
-    visitor = Visitor()
-    visitor.parse_header(opts['<input>'], opts['<clang_args>'])
-    print(json.dumps(visitor.defs, indent=None if opts.get('--compact') else 2))
+    definitions = definitions_from_header(
+        opts['<input>'], opts['<clang_args>'])
+    print(json.dumps(definitions, indent=None if opts.get('--compact') else 2))
 
 
 if __name__ == '__main__':
